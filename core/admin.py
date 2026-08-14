75c907567a0e73830d15163b8b936f9d9791670f
Сеньор Берлин <user@userui-MacBookPro.local>
2026-08-14 15:23:47 +0600

    Use Unfold admin for auth models

--- core/admin.py
+++ core/admin.py
@@ -1,7 +1,24 @@
 from django.contrib import admin
+from django.contrib.auth.admin import GroupAdmin as DjangoGroupAdmin
+from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
+from django.contrib.auth.models import Group, User
 from unfold.admin import ModelAdmin, TabularInline
 
 from .models import AppSettings, Product, ProductSize, Store, Transaction, TransactionItem
+
+
+admin.site.unregister(User)
+admin.site.unregister(Group)
+
+
+@admin.register(User)
+class UserAdmin(DjangoUserAdmin, ModelAdmin):
+    pass
+
+
+@admin.register(Group)
+class GroupAdmin(DjangoGroupAdmin, ModelAdmin):
+    pass
 
 
 @admin.register(Store)
